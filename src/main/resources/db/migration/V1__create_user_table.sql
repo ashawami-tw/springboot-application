12f7ced2d14eed31911379868064c1c567852881
Ashawami <ashawami.mulik@thoughtworks.com>
2023-05-05 08:16:13 +0530

    Add migration files

---
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+CREATE TABLE IF NOT EXISTS "user"
+(
+    id              uuid DEFAULT public.uuid_generate_v4() UNIQUE NOT NULL PRIMARY KEY,
+    password        VARCHAR(255) NOT NULL,
+    email           VARCHAR(255) NOT NULL UNIQUE,
+    created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
